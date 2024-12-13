8453c2ca513101c728183ce6df2aa44496d0c067
Guilherme1Souza <guilhermeeduardo2016@outlook.com>
2024-12-13 16:03:00 -0300

    "feat: Adicionando os componentes ButtonText, Tag e finalizando a page Details"

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,26 +1,42 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 
 import { Header } from '../../components/Header'
+import { ButtonText } from '../../components/ButtonText'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/tag'
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">https://rocketseat.com.br</a>
-          </li>
-          <li>
-            <a href="#">https://rocketseat.com.br</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir a nota" />
 
-      <Button title="Voltar" />
+          <h1>Introdução ao React</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
