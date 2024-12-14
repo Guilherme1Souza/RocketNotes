a6840c0a95276ce61f59387118e9291f743b20a0
Guilherme1Souza <guilhermeeduardo2016@outlook.com>
2024-12-14 13:57:46 -0300

    "feat: estilizando os botões e inserindo o isActivate"

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { Container, Brand, Menu, Search, Content, NewNote } from './style'
 
 
 import { Header } from '../../components/Header'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Home(){
     return (
@@ -16,7 +17,9 @@ export function Home(){
             </Header>
 
             <Menu>
-
+            <li><ButtonText title="Todos" isActive/></li>
+            <li><ButtonText title="React"/></li>
+            <li><ButtonText title="NodeJs"/></li>
             </Menu>
 
             <Search>
