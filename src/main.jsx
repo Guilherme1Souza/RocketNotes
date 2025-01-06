3c041f0e172746eec671a6e41a7cb1c55e9bd3e4
Guilherme Souza <guilhermeeduardo2016@outlook.com>
2025-01-06 17:00:05 -0300

    Construção da page SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Home } from './pages/Home';
+import { SignIn } from './pages/SignIn';
 
 import { ThemeProvider } from 'styled-components';
 import  GlobalStyles from './styles/global';
@@ -11,7 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-    <Home/>
+    <SignIn/>
     </ThemeProvider>
   </StrictMode>,
 )
