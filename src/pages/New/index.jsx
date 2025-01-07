bfde51c7f4abf431ed578734a11cf8ac770af79a
Guilherme Souza <guilhermeeduardo2016@outlook.com>
2025-01-06 23:29:27 -0300

    Finalizando o componente NoteItem e a página New

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,25 +1,42 @@
-import { Header } from "../../components/Header"
-import { Input } from "../../components/Input"
-import { TextArea } from "../../components/TextArea"
+import { TextArea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
 
-import { Container, Form } from "./style"
+import { Container, Form } from './style'
 
 export function New() {
-    return (
-        <Container>
-            <Header/>
-            <main>
-                <Form>
-                    <header>
-                        <h1>Criar nota</h1>
-                        <a href="/">Voltar</a>
-                    </header>
+  return (
+    <Container>
+      <Header />
 
-                    <Input placeholder="Título"/>
-                    <TextArea placeholder="Observações"/>
-                </Form>
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
 
-            </main>
-        </Container>
-    )
+          <Input placeholder="Título" />
+          <TextArea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
+        </Form>
+      </main>
+    </Container>
+  )
 }
